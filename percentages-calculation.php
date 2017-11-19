9755540967f6b1e88120436b8f9bd5e6eddfca59
viery365 <20521892+viery365@users.noreply.github.com>
2017-11-19 19:54:50 +0000

    Minor cleanup
    * Re-located PHP code to the top of the file
    * Reduced number of functions to 1

--- percentages-calculation.php
+++ percentages-calculation.php
@@ -1,10 +1,34 @@
+<?php
+
+if(isset($_POST['submit']) && !empty($_POST['input-value'])){
+
+  ob_start();
+
+    $inputValue = $_POST['input-value'];
+    echo '<h1>Input Value ' . $inputValue. '</h1>';
+
+    function getSavings($n, $inputValue){
+      return round(($n / 100) * $inputValue, 2);
+    }
+
+    $remaining;
+
+    for($i = 1; $i <= 100; $i++){
+      $remaining = $inputValue - getSavings($i, $inputValue);
+      $calculation = '<h3>Savings: ' . $i . '% = ' . getSavings($i, $inputValue) . ' Mimos: ' . $remaining . '</h3>';
+      echo $calculation;
+    }
+
+    $output = ob_get_clean();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Percentages Calculation</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
   </head>
   <body>
     <form method="post" action="">
@@ -12,22 +36,6 @@
         <input id="input-value" type="text" name="input-value">
         <input type="submit" name="submit" value="Check Percentages">
     </form>
-    <?php if(isset($_POST['submit']) && !empty($_POST['input-value'])):
-      $inputValue = $_POST['input-value'];
-      echo '<h1>Input Value ' . $inputValue. '</h1>';
-
-      function getSavings($n, $inputValue){
-        return round(($n / 100) * $inputValue, 2);
-      }
-
-      function getMimos($n, $inputValue){
-        $savings = getSavings($n, $inputValue);
-        return $inputValue - $savings;
-      }
-      for($i = 1; $i <= 100; $i++){
-        echo '<h2>Savings: ' . $i . '% = ' . getSavings($i, $inputValue) . ' Mimos: ' . getMimos($i, $inputValue) . '</h2>';
-      }
-      ?>
-  <?php endif; ?>
+    <?=$output ?? ''?>
   </body>
 </html>
