297d0aad54146ad6c7287b970ac504260f147435
viery365 <20521892+viery365@users.noreply.github.com>
2017-11-20 23:46:19 +0000

    Added percentages-calculation view

--- percentages-calculator/index.php
+++ percentages-calculator/index.php
@@ -22,20 +22,4 @@ if(isset($_POST['submit']) && !empty($_POST['input-value'])){
     $output = ob_get_clean();
 }
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Percentages Calculation</title>
-  </head>
-  <body>
-    <form method="post" action="">
-      <label for="input-value">LEFT OVER:</label>
-        <input id="input-value" type="text" name="input-value">
-        <input type="submit" name="submit" value="Check Percentages">
-    </form>
-    <?=$output ?? ''?>
-  </body>
-</html>
+require 'percentages-calculation.view.php';
---
+++ percentages-calculator/percentages-calculation.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Percentages Calculation</title>
+  </head>
+  <body>
+    <form method="post" action="">
+      <label for="input-value">LEFT OVER:</label>
+        <input id="input-value" type="text" name="input-value">
+        <input type="submit" name="submit" value="Check Percentages">
+    </form>
+    <?=$output ?? ''?>
+  </body>
+</html>
