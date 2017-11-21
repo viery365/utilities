d94bcef7d39bfcd78c9a37231434f12d86050f07
viery365 <20521892+viery365@users.noreply.github.com>
2017-11-21 09:20:15 +0000

    Added functions file
    Some functions to add to a mini personal framework in the future

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+//Some functions to add to a mini personal framework in the future
+
+//pre print_r()
+function pp($data){
+  echo '<pre>';
+    print_r($data);
+  echo '<pre>';
+}
+
+//pre var_dump(
+function pd($data){
+  echo '<pre>';
+    var_dump($data);
+  echo '<pre>';
+}
